5bd798ec69766974fae704441b0d248a6bedbbdc
ShadowGabbo <gabriele.sarti1@studenti.unimi.it>
2022-12-15 21:18:36 +0100

    refactoring using struct and enums completed

--- 02_aoc_2022/main.go
+++ 02_aoc_2022/main.go
@@ -30,28 +30,51 @@ func main() {
 	// part one
 	scanner := bufio.NewScanner(file)
 	total_points := 0
-	rock_paper_scissor := map[string]int{"AA": 4, "BB": 5, "CC": 6, "AB": 1, "AC": 7, "BA": 8, "BC": 2, "CA": 3, "CB": 9}
+	rock_paper_scissor := map[turn]int{
+		{my_choice: 'A', opponent_choice: 'A'}: 4,
+		{my_choice: 'B', opponent_choice: 'B'}: 5,
+		{my_choice: 'C', opponent_choice: 'C'}: 6,
+		{my_choice: 'A', opponent_choice: 'B'}: 1,
+		{my_choice: 'A', opponent_choice: 'C'}: 7,
+		{my_choice: 'A', opponent_choice: 'A'}: 4,
+		{my_choice: 'B', opponent_choice: 'A'}: 8,
+		{my_choice: 'B', opponent_choice: 'C'}: 2,
+		{my_choice: 'C', opponent_choice: 'A'}: 3,
+		{my_choice: 'C', opponent_choice: 'B'}: 9}
 	lines := []string{}
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		lines = append(lines, line)
 		opponent_choice := line[0]
 		my_choice := line[2] - 23 // so i can use rune as numbers
-		// turn := turn{my_choice: Play(my_choice), opponent_choice: Play(opponent_choice)}
-		total_points += rock_paper_scissor[string(my_choice)+string(opponent_choice)]
+		turn := turn{my_choice: Play(my_choice), opponent_choice: Play(opponent_choice)}
+		total_points += rock_paper_scissor[turn]
 	}
 	fmt.Printf("Day two aoc 2022\n")
 	fmt.Printf("Part one: %d\n", total_points)
 
-	// part two
+	//part two
 	total_points2 := 0
-	rock_paper_scissor2 := map[string]string{"AA": "C", "BB": "B", "CC": "A", "AB": "A", "AC": "B", "BA": "A", "BC": "C", "CA": "B", "CB": "C"}
+	rock_paper_scissor2 := map[turn]rune{
+		{my_choice: 'A', opponent_choice: 'A'}: 'C',
+		{my_choice: 'B', opponent_choice: 'B'}: 'B',
+		{my_choice: 'C', opponent_choice: 'C'}: 'A',
+		{my_choice: 'A', opponent_choice: 'B'}: 'A',
+		{my_choice: 'A', opponent_choice: 'C'}: 'B',
+		{my_choice: 'B', opponent_choice: 'A'}: 'A',
+		{my_choice: 'B', opponent_choice: 'C'}: 'C',
+		{my_choice: 'C', opponent_choice: 'A'}: 'B',
+		{my_choice: 'C', opponent_choice: 'B'}: 'C'}
 
 	for _, line := range lines {
 		opponent_choice2 := line[0]
 		condition := line[2] - 23 // so i can use rune as numbers
-		my_choice2 := rock_paper_scissor2[string(opponent_choice2)+string(condition)]
-		total_points2 += rock_paper_scissor[string(my_choice2)+string(opponent_choice2)]
+		get_choice := turn{my_choice: Play(opponent_choice2), opponent_choice: Play(condition)}
+		my_choice2 := rock_paper_scissor2[get_choice]
+		turn := turn{my_choice: Play(my_choice2), opponent_choice: Play(opponent_choice2)}
+		total_points2 += rock_paper_scissor[turn]
 	}
 	fmt.Printf("Part two: %d\n", total_points2)
+
 }
